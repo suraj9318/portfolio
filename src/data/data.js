a105f3908e670eed82f0c72642bb3ac2dee8a2c8
suraj <surajmishra9318@gmail.com>
2022-12-24 12:43:00 +0530

    initial push

---
+++ src/data/data.js
@@ -0,0 +1,47 @@
+import confySloth from '../assets/projects/comfy-sloth.png'
+import githubUser from '../assets/projects/github-user.png'
+import stockImg from '../assets/projects/stock-img.png'
+import quizApp from '../assets/projects/quiz-app.png'
+import movieDb from '../assets/projects/movies-db.png'
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:confySloth,
+        github:"https://github.com/suraj9318/comfy-sloth-e-com",
+        live:"https://e-comfy-sloth.netlify.app",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:githubUser,
+        github:"https://github.com/suraj9318/search-github-users",
+        live:"https://search-github-user-information.netlify.app",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:stockImg,
+        github:"https://github.com/suraj9318/stock-image",
+        live:"https://stock-image.netlify.app",
+    },
+    {
+        id:4,
+        name:"React JS Application",
+        image:quizApp,
+        github:"https://github.com/suraj9318/quiz-app",
+        live:"https://playquizdaily.netlify.app/",
+    },
+    {
+        id:5,
+        name:"React JS Application",
+        image:movieDb,
+        github:"https://github.com/suraj9318/movie-db",
+        live:"https://moviesbits.netlify.app/",
+    },
+
+
+    
+
+]
