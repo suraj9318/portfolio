bbac95b49d175fbed27eca8ba8cb69d6bda14bce
suraj <surajmishra9318@gmail.com>
2022-12-24 20:14:34 +0530

    footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='w-full h-[50px] bg-[#060f20] text-gray-300 shadow-slate-300'>
+        <p className='text-center pt-3'>@portlify, Inc. All rights reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
