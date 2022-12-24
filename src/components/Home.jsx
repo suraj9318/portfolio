4f9e7a04d538d257897eeffe419ba35d327e5552
suraj <surajmishra9318@gmail.com>
2022-12-24 20:22:29 +0530

    home modifications

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,9 +14,7 @@ const Home = () => {
           I'm a Front-End Developer.
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
-          I’m a full-stack developer specializing in building (and occasionally
-          designing) exceptional digital experiences. Currently, I’m focused on
-          building responsive full-stack web applications.
+        I am a enthusiastic front-end developer and have been in the industry for more than 1.4 years. I have a keen eye for detail and most of my working life has been dedicated to creating stunning websites for clients.
         </p>
         <div>
         <Link to='work' smooth={true} duration={500}>
