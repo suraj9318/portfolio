f961e5310e70e6197bb031501d8205ad8198e1f1
suraj <surajmishra9318@gmail.com>
2022-12-25 18:00:09 +0530

    back to normal

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -96,7 +96,7 @@ const Navbar = () => {
       </ul>
 
       {/* Social icons */}
-      <div className=' lg:flex fixed flex-col top-[35%] left-0'>
+      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
         <ul>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a
