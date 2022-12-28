9481d48d0469eb820d95978a77b874a303c9b42b
suraj <surajmishra9318@gmail.com>
2022-12-28 19:51:19 +0530

    footer modification

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='w-full h-[50px] bg-[#060f20] text-gray-300 shadow-slate-300'>
-        <p className='text-center pt-3'>@portlify, Inc. All rights reserved.</p>
+        <p className='text-center pt-3'>@suraj9318, Inc. All rights reserved.</p>
     </footer>
   )
 }
